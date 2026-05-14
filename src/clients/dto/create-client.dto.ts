94ffc6279ea9b5ad415a88adb8dcd873c0812aef
gui200428 <gui200428@gmail.com>
2026-05-14 01:01:43 -0300

    Responsavel

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,50 +1,55 @@
 import {
-  IsString,
-  IsEmail,
-  IsOptional,
-  IsUrl,
-  IsEnum,
-  MaxLength,
+    IsString,
+    IsEmail,
+    IsOptional,
+    IsUrl,
+    IsEnum,
+    MaxLength,
 } from 'class-validator';
 import { ClientStatus } from '@prisma/client';
 
 export class CreateClientDto {
-  @IsString()
-  @MaxLength(255)
-  name: string;
+    @IsString()
+    @MaxLength(255)
+    name: string;
 
-  @IsOptional()
-  @IsString()
-  @MaxLength(255)
-  ownerName?: string;
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    ownerName?: string;
 
-  @IsEmail()
-  email: string;
+    @IsEmail()
+    email: string;
 
-  @IsOptional()
-  @IsString()
-  @MaxLength(50)
-  phone?: string;
+    @IsOptional()
+    @IsString()
+    @MaxLength(50)
+    phone?: string;
 
-  @IsUrl()
-  website: string;
+    @IsUrl()
+    website: string;
 
-  @IsOptional()
-  @IsString()
-  @MaxLength(100)
-  country?: string;
+    @IsOptional()
+    @IsString()
+    @MaxLength(100)
+    country?: string;
 
-  @IsOptional()
-  @IsString()
-  @MaxLength(50)
-  language?: string;
+    @IsOptional()
+    @IsString()
+    @MaxLength(50)
+    language?: string;
 
-  @IsOptional()
-  @IsString()
-  @MaxLength(2000)
-  notes?: string;
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    negotiator?: string;
 
-  @IsOptional()
-  @IsEnum(ClientStatus)
-  status?: ClientStatus;
+    @IsOptional()
+    @IsString()
+    @MaxLength(2000)
+    notes?: string;
+
+    @IsOptional()
+    @IsEnum(ClientStatus)
+    status?: ClientStatus;
 }
